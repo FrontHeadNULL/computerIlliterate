e58937cc1a8189d73515a09322404eb9dcbfed33
Jung Sol <solhsw@naver.com>
2022-09-25 22:58:19 +0900

    sol_first_commit

---
+++ today_house/src/test/java/com/gap/today_house/TodayHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gap.today_house;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
